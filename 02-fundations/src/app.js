6d6b4f1f6f85bc570292a7e231d1764b5ecc4342
Leidi De La Puente <leididelapuente3@gmail.com>
2024-10-12 12:47:31 -0500

    factory functions and adapter pattern

--- 02-fundations/src/app.js
+++ 02-fundations/src/app.js
@@ -1,5 +1,5 @@
 import {getAge, getId} from "./plugins/index.js";
-import {buildPerson} from "./1.js";
+import {buildPerson} from "./factory.js";
 
 const makePerson = buildPerson({getAge, getId})
 const person = makePerson({name: 'Aaron', birthDate: '10-8-5'})
